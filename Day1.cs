0abcc2827db6641ff6c2fbd1abff46eeda2a78e6
mgk <66747844+truemgk@users.noreply.github.com>
2023-12-03 15:11:16 +0100

    Add project files.

---
+++ Day1.cs
@@ -0,0 +1,63 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode
+{
+    [Day(1, "Trebuchet")]
+    internal class Day1
+    {
+        [Solution("Regex - Part one")]
+        public static string Regx(string input)
+        {
+            Regex regex = new(@"[^0-9]");
+
+            var lines = input.Split("\n");
+            var total = 0;
+            foreach (var line in lines)
+            {
+                var mline = regex.Replace(line, "");
+                var dub = "" + mline.First() + mline.Last();
+                var num = int.Parse(dub);
+                total += num;
+            }
+
+            return "" + total;
+        }
+
+        [Solution("Trebuchet - Regex - now with words")]
+        public static string SolutionPart2(string input)
+        {
+            var lines = input.Split("\n");
+            var total = 0;
+            Regex numbers = new(@"[^0-9]");
+            Regex one = new(@"one");
+            Regex two = new(@"two");
+            Regex three = new(@"three");
+            Regex four = new(@"four");
+            Regex five = new(@"five");
+            Regex six = new(@"six");
+            Regex seven = new(@"seven");
+            Regex eight = new(@"eight");
+            Regex nine = new(@"nine");
+
+            foreach (var line in lines)
+            {
+                var mline = one.Replace(line, "1");
+                mline = two.Replace(mline, "2");
+                mline = three.Replace(mline, "3");
+                mline = four.Replace(mline, "4");
+                mline = five.Replace(mline, "5");
+                mline = six.Replace(mline, "6");
+                mline = seven.Replace(mline, "7");
+                mline = eight.Replace(mline, "8");
+                mline = nine.Replace(mline, "9");
+                mline = numbers.Replace(mline, "");
+
+                var dub = "" + mline.First() + mline.Last();
+                var num = int.Parse(dub);
+                total += num;
+            }
+
+            return "" + total;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿
+//find all classes with DayAttribute that have a method with SolutionAttribute and run it
+using System.Reflection;
+
+var types = Assembly.GetExecutingAssembly().GetTypes()
+    .Where(t => t.GetCustomAttributes(typeof(DayAttribute), false).Length > 0)
+    .Where(t => t.GetMethods().Any(m => m.GetCustomAttributes(typeof(SolutionAttribute), false).Length > 0));
+
+foreach (var type in types)
+{
+    var day = type.GetCustomAttribute<DayAttribute>().Day;
+    var name = type.GetCustomAttribute<DayAttribute>().Name;
+    var input = DayInputLoader.Load(day);
+    var solutions = type.GetMethods().Where(m => m.GetCustomAttributes(typeof(SolutionAttribute), false).Length > 0);
+    //write the header
+    Console.WriteLine($"Day {day} - {name}");
+
+    foreach (var solution in solutions)
+    {
+        var solutionName = solution.GetCustomAttribute<SolutionAttribute>().Name;
+        Console.WriteLine($"Solution - {solutionName}");
+        Console.WriteLine(solution.Invoke(null, new object[] { input }));
+        Console.WriteLine("===========================================");
+    }
+}
+
+
+class DayAttribute : Attribute
+{
+    public DayAttribute(int day, string name)
+    {
+        Day = day;
+        Name = name;
+    }
+
+    public int Day { get; }
+    public string Name { get; }
+}
+
+class SolutionAttribute : Attribute
+{
+    public SolutionAttribute(string name)
+    {
+        Name = name;
+    }
+
+    public string Name { get; }
+}
+
+class DayInputLoader
+{
+    public static string Load(int day)
+    {
+        //load input file
+        return System.IO.File.ReadAllText($"../../../inputs/day{day}.txt");
+    }
+}
